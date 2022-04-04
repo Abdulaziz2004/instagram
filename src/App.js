1a0081a1fa01fb344bcee45514ad96b05b168dd9
Abdulaziz2004 <abu556626@gmail.com>
2022-04-04 16:37:13 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {Routes, Route} from 'react-router-dom'
+import Login from './pages/Login'
+import SignIn from './pages/SignIn'
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/signIn' element={<SignIn/>}/>
+      </Routes>
     </div>
   );
 }
