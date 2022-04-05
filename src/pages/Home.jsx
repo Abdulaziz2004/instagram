e9ac8dc150a2d6ce90993deea442fd356d01a9f8
umarismaildjanoff@gmail.com <umarismaildjanoff@gmail.com>
2022-04-05 14:13:56 +0500

    getting update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      home
-    </div>
+    <Wrapper>
+      hello      
+    </Wrapper>
   );
 };
 
 export default Home;
+
+const Wrapper = styled.div``;
